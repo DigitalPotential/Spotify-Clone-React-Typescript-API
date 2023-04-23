7fa250ca5c5ee5540f6ae89e1bd84b64b65d5f6a
DigitalPotential <Carlos@digitalpotential.se>
2023-04-23 20:36:37 +0200

    Added functionality to player controls

--- src/components/PlayerControls/PlayerControls.tsx
+++ src/components/PlayerControls/PlayerControls.tsx
@@ -5,38 +5,55 @@ import { useState } from 'react';
 import { SpotifyPlayer } from '../../Types/spotify-types';
 
 interface PlayerControlsProps {
-    is_paused: boolean;
-    duration: number | null;
-    progress: number | null;
-    player: SpotifyPlayer | null;
-  }
-  
+	is_paused: boolean;
+	duration: number | null;
+	progress: number | null;
+	player: SpotifyPlayer | null;
+}
 
-  const PlayerControls = ({
-    is_paused,
-    duration,
-    progress,
-    player,
-  }: PlayerControlsProps) => {
+const PlayerControls = ({ is_paused, duration, progress, player }: PlayerControlsProps) => {
+	const [currentProgress, setCurrentProgress] = useState(progress);
 	const skipStyle = { width: 28, height: 28 };
 	const playStyle = { width: 38, height: 38 };
 	return (
 		<Stack direction="column" spacing={2} justifyContent="center" alignItems="center" sx={{ width: '100%' }}>
 			<Stack spacing={1} direction={'row'} justifyContent={'center'} alignItems={'center'} sx={{ width: '100%' }}>
-				<IconButton size="small" sx={{ color: 'text.primary' }}>
+				<IconButton
+					size="small"
+					sx={{ color: 'text.primary' }}
+					onClick={() => {
+						setCurrentProgress(0);
+						player.previousTrack();
+					}}
+				>
 					<SkipPrevious sx={skipStyle} />
 				</IconButton>
-				<IconButton size="small" sx={{ color: 'text.primary' }}>
+				<IconButton
+					size="small"
+					sx={{ color: 'text.primary' }}
+					onClick={() => {
+						player.togglePlay();
+					}}
+				>
 					{is_paused ? <PlayArrow sx={playStyle} /> : <Pause sx={playStyle} />}
 				</IconButton>
-				<IconButton size="small" sx={{ color: 'text.primary' }}>
+				<IconButton
+					size="small"
+					sx={{ color: 'text.primary' }}
+					onClick={() => {
+						setCurrentProgress(0);
+						player.nextTrack();
+					}}
+				>
 					<SkipNext sx={skipStyle} />
 				</IconButton>
 			</Stack>
 			<Stack spacing={2} direction="row" justifyContent={'center'} alignItems={'center'} sx={{ width: '75%' }}>
-            <Typography sx={{color: 'text.secondary', fontSize: 12}}>{formatTime(progress ?? 0)}</Typography>
-            <Slider max={duration} value={progress} min={0} size={'medium'} />
-            <Typography sx={{color: 'text.secondary', fontSize: 12}}>{formatTime(duration ?? 0)}</Typography>
+				<Typography sx={{ color: 'text.secondary', fontSize: 12 }}>
+					{formatTime(setCurrentProgress ?? 0)}
+				</Typography>
+				<Slider max={duration} value={progress} min={0} size={'medium'} />
+				<Typography sx={{ color: 'text.secondary', fontSize: 12 }}>{formatTime(duration ?? 0)}</Typography>
 			</Stack>
 		</Stack>
 	);
