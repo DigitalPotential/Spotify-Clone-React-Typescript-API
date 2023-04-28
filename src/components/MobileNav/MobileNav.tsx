56bf66f0bdd1ae13d72196e6973a6065d38364a2
DigitalPotential <Carlos@digitalpotential.se>
2023-04-29 01:29:03 +0200

    feat: MobileNav

--- src/components/MobileNav/MobileNav.tsx
+++ src/components/MobileNav/MobileNav.tsx
@@ -5,15 +5,18 @@ import { useNavigate } from 'react-router-dom';
 
 const MobileNav = () => {
     const navigate = useNavigate();
+    const [value, setValue] = useState<number>(0);
+
 	return (
 		<Box sx={{ display: { xs: 'block', md: 'none' } }}>
 			<BottomNavigation
-				sx={{ backgroundColor = 'background.paper' }}
+				sx={{ backgroundColor: 'background.paper' }}
 				showLabels
-				value={null}
-				onChange={null}
+				value={value}
+				onChange={(event, newValue) => {setValue(newValue);}}
 			>
                 <BottomNavigationAction label="Home" icon={<Home />} onClick={() => navigate('/')} />
+                <BottomNavigationAction label="Ditt bibliotek" icon={<List />} onClick={() => navigate('/library')} />
             </BottomNavigation>
 		</Box>
 	);
